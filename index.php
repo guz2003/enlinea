828ee0d62d271dad1b763c0a19c1ec9eef23dc5d
unknown <noylyguzmanalva@gmail.com>
2024-10-02 17:34:00 -0500

    ENLINEA

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="resources/css/main.css">
+</head>
+<body class="px-3">
+    <div class="row mt-5">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right pr-5">soluciones mundiales</p>
+        </div>
+    </div>
+    <!-- fin de encabezado -->
+     <div class="row">
+        <div class="col-sm-12">
+            <img class="w-100" style="height: 350px;" src="./resources/img/baner01.PNG" alt="">
+        </div>
+     </div>
+     <div class="container mt-4">
+        <h1 class="h1">SOLUCIONES CREATIVAS</h1>
+        <p>sin importar cual es tu negocio, nuestro equipo puede hacerlo desde cero.</p>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_01.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-pen-fill"></i>  DISEÑO</h5>
+                        <p>creamos diseño segun tus necesidades.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-flower1"></i>  DESARROLLO</h5>
+                        <p>nuestros programadores te garantizaran la perfección.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-patch-plus-fill"></i> EDICION DE LIBROS</h5>
+                        <p>Que el producto final llegue a tus clientes.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+     <footer class="bg-wine">
+        <p class="p-5 text-white">Edificio IDEX PJ, jr. Amazonas 120 - chachapoyas </p>
+     </footer>
+</body>
+</html>
